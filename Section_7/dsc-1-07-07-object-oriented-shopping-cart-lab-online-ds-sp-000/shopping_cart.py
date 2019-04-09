5dbaf48009371907c34f3a3e41ee98b07526641b
adpaz <pazadrian08@gmail.com>
2019-04-08 23:59:00 -0600

    Finished shopping cart

--- Section_7/dsc-1-07-07-object-oriented-shopping-cart-lab-online-ds-sp-000/shopping_cart.py
+++ Section_7/dsc-1-07-07-object-oriented-shopping-cart-lab-online-ds-sp-000/shopping_cart.py
@@ -1,11 +1,22 @@
 class ShoppingCart:
     # write your code here
     def __init__(self, emp_discount=None):
-      pass
+        self.total = 0
+        self.emp_discount = emp_discount
+        self.items = []
     def add_item(self, name, price, quantity=1):
-       pass
+        self.items.append([name, price, quantity])
+        self.total = 0
+        for i in range(len(self.items)):
+            self.total += self.items[i][1] * self.items[i][2]
+        return self.total
     def mean_item_price(self):
-       pass
+        quant = 0
+        for i in range(len(self.items)):
+            quant += self.items[i][2]
+        total = self.total
+        mean = total/quant
+        return mean
 
     def median_item_price(self):
         pass
