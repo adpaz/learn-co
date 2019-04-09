ddbaafb7a26f4312de9c3c4340419a365342e6f2
adpaz <pazadrian08@gmail.com>
2019-04-09 00:21:21 -0600

    updated shopping cart

--- Section_7/dsc-1-07-07-object-oriented-shopping-cart-lab-online-ds-sp-000/shopping_cart.py
+++ Section_7/dsc-1-07-07-object-oriented-shopping-cart-lab-online-ds-sp-000/shopping_cart.py
@@ -19,10 +19,30 @@ class ShoppingCart:
         return mean
 
     def median_item_price(self):
-        pass
+        
+        length = 0
+        for i in range(len(self.items)):
+            length += self.items[i][2]
+        if (length%2 == 0):
+            mid_one = int(length/2)
+            mid_two = mid_one - 1
+            median = (prices[mid_one] + prices[mid_two])/2
+            return median
+        mid = int(length/2)
+        return self.items[mid][1]
 
     def apply_discount(self):
-       pass
+        if (self.emp_discount == None):
+            return "Sorry, there is no discount to apply to your cart :("
+        else:
+            return self.total*0.8
 
     def void_last_item(self):
-        pass
+        if self.items:
+            self.items.pop()
+        else:
+            return "no items"
+        self.total = 0
+        for i in range(len(self.items)):
+            self.total += self.items[i][1] * self.items[i][2]
+        return self.total
